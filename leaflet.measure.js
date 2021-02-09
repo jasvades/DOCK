a87bdd20a4e95afcfd05e5ab465ee6fa18e02e84
jasvades <76697910+jasvades@users.noreply.github.com>
2021-02-09 02:49:24 +0100

    enable download mobile after measure

--- leaflet.measure.js
+++ leaflet.measure.js
@@ -70,7 +70,7 @@ L.Control.Measure = L.Control.extend({
 		if (this._measuring) {
 			L.DomUtil.addClass(this._container, 'leaflet-control-measure-on')
 			this._startMeasuring()
-		} else if (gpx && !(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))) {
+		} else if (gpx) {
 			var filename = prompt('Give the filename to store the GPX file');
 			if (filename != null){
 				var gpx_file = togpx(this._layerPaint.toGeoJSON());
